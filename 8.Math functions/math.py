62d20a841db666f33b4a2114e4131c41046d4f4c
MuGee <jamesjim908@gmail.com>
2022-12-08 23:22:48 +0300

    added math functions

---
+++ 8.Math functions/math.py
@@ -0,0 +1,12 @@
+# Math Functions
+x = 2.9
+print(round(x))
+
+# Absolute(returns absolute values)
+print(abs(-2.9))
+
+# For complex math operations the math module is required
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.sqrt(4))
