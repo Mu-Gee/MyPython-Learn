8fc0234c33e9694529b1707a5c5420815f574e2c
MuGee <jamesjim908@gmail.com>
2022-12-08 22:34:19 +0300

    updated Int operations

--- 3.Int operations/int_operations.py
+++ 3.Int operations/int_operations.py
@@ -1,5 +1,4 @@
- 
-# Operations on int and type casting
+ # Operations on int and type casting
 weight_pounds = input('Weight (lbs): ')
 weight_kgs = int(weight_pounds) * 0.45 #typecasting
 print(weight_kgs)
