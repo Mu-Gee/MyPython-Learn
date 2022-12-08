429cf651f3183c033d643c2a47d6d2a790f99b23
MuGee <jamesjim908@gmail.com>
2022-12-08 23:21:19 +0300

    added arithmetic operators

---
+++ 7.Arithmetic operators/arithmetic_ops.py
@@ -0,0 +1,32 @@
+# Arithmetic operators
+print(10+3)
+print(10-3)
+print(10*3)
+
+# Gives a floating point number
+print(10/3)
+
+# Gives a whole integer number
+print(10//3)
+
+# Gives remainder of the division
+print(10 % 3)
+
+# This is the exponent operator
+print(10**3)
+
+# Incrementing/decrementing value
+x = 10
+x = x+3
+print(x)
+
+# Augmented assignment operator +=/-=
+x = 10
+x += 3
+print(x)
+
+# Operator precedence ()->(**)->(*/)->(+-)
+x = 10 + 3 * 2 ** 2
+y = (2+3) * 10 - 2
+print(x)
+print(y)
