c6d2e9f44cc35f02f94c9c14ceb3a2b54e12e7a1
MuGee <jamesjim908@gmail.com>
2022-12-08 22:32:09 +0300

    added formatted strings

---
+++ 5.Formatted strings/formatted_str.py
@@ -0,0 +1,11 @@
+# Formatted strings
+first = 'James'
+last = 'Mugambi'
+
+# Non-formatted string
+message = first + ' [' + last + '] is a coder'
+
+# Using a formatted string
+msg = f'{first} [{last}] is a coder'
+print(message)
+print(msg)
