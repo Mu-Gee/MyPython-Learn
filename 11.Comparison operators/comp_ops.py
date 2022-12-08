7703244a225e09bbfa17fc97bb838abe5547ab4a
MuGee <jamesjim908@gmail.com>
2022-12-08 23:30:06 +0300

    added comparison operators

---
+++ 11.Comparison operators/comp_ops.py
@@ -0,0 +1,20 @@
+# Comparison Operators
+temperature = 30
+# Greater than >
+if temperature > 30:
+    print("It's a very hot day")
+else:
+    print("It's not a hot day")
+# Less than <
+# Equal to ==
+# Greater than or equal to >=
+# Less than or equal to <=
+# Not equal to !=
+
+name = "J"
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 characters")
+else:
+    print("Name looks good")
