7e3636b96d0094464f9711eec4a3b14f72917f7a
Mu-Gee <jamesjim908@gmail.com>
2023-05-31 13:42:10 +0300

    Two dimensional lists

---
+++ 016.2D Lists/2D.py
@@ -0,0 +1,11 @@
+#2D list is a list of lists
+matrix = [
+    [1, 2, 2],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0])#this is the first list(item) in the matrix list
+#access individual item
+#[0]index of first list[1]index of item in the first list
+print(matrix[0][1])
